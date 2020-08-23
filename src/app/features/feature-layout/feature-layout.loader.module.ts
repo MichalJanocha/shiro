1a9d027cafdb7c037911fb32b8630bc9d2a4c36e
Jekorgi <michaljanocha98@gmail.com>
2020-08-23 20:36:16 +0200

    Build test

--- src/app/features/feature-layout/feature-layout.loader.module.ts
+++ src/app/features/feature-layout/feature-layout.loader.module.ts
@@ -1,17 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { FeatureLayoutComponent } from './layout-page/feature-layout.component';
-import { RouterModule } from '@angular/router';
 import {FeatureLayoutRoutingModule} from './feature-layout.routing.module';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { LayoutUserComponent } from './layout-user/layout-user.component';
+import { LayoutNotificationsComponent } from './layout-notifications/layout-notifications.component';
 
 const MODULES = [
     FeatureLayoutRoutingModule, IonicModule, CommonModule
 ]
 
 const COMPONENTS = [
-    FeatureLayoutComponent
+    FeatureLayoutComponent,
+    LayoutUserComponent,
+    LayoutNotificationsComponent
 ]
 
 @NgModule({
--- src/app/features/feature-sign/feature-sign.loader.module.ts
+++ src/app/features/feature-sign/feature-sign.loader.module.ts
@@ -2,9 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { SignPageComponent } from './sign-page/sign-page.component';
+import { CommonModule } from '@angular/common';
 
 const MODULES = [
     RouterModule.forChild([{ path: '', component: SignPageComponent }]),
+    CommonModule
 ]
 
 const COMPONENTS = [
